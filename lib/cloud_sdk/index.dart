faa600a7a2c456d1111aacf94b22e7a2ad255632
RetroAstro <452461114@qq.com>
2021-03-04 21:22:45 +0800

    chore: refactor server code

--- lib/cloud_sdk/index.dart
+++ lib/cloud_sdk/index.dart
@@ -1,11 +1,31 @@
-import 'package:flutter/material.dart';
+import 'package:leancloud_storage/leancloud.dart';
+import 'package:server/storage/user.dart';
 
 class CloudSDK {
   final String username;
 
-  CloudSDK({
-    @required this.username,
-  });
+  CloudSDK({this.username});
 }
 
-final CloudSDK cloudSDK = CloudSDK(username: '');
+class RegisterErrorHandler {
+  final Map<int, Function> handler = Map();
+
+  RegisterErrorHandler() {
+    handler[202] = usernameError;
+    handler[203] = emailError;
+    handler[214] = mobileError;
+  }
+
+  void usernameError() {}
+  void emailError() {}
+  void mobileError() {}
+}
+
+void test() {
+  register().catchError((e) {
+    if (e is LCException) {
+      final handle = RegisterErrorHandler().handler[e.code];
+      handle();
+    }
+  });
+}
---
+++ server/lib/storage/user.dart
@@ -0,0 +1,64 @@
+import 'package:leancloud_storage/leancloud.dart';
+
+Future<void> register({
+  String username,
+  String password,
+  String email,
+  String mobile,
+  String gender,
+}) async {
+  final LCUser user = LCUser();
+
+  user
+    ..username = username
+    ..password = password
+    ..email = email
+    ..mobile = mobile;
+  user['gender'] = gender;
+
+  await user.signUp();
+}
+
+Future<void> loginByUsernamePassword(String username, String password) async {
+  await LCUser.login(username, password);
+}
+
+Future<void> loginByEmailPassword(String email, String password) async {
+  await LCUser.login(email, password);
+}
+
+Future<void> loginByMobilePassword(String mobile, String password) async {
+  await LCUser.login(mobile, password);
+}
+
+Future<void> requestLoginSMSCode(String mobile) async {
+  await LCUser.requestLoginSMSCode(mobile);
+}
+
+Future<void> loginByMobileSMSCode(String mobile, String smscode) async {
+  await LCUser.signUpOrLoginByMobilePhone(mobile, smscode);
+}
+
+Future<void> requestMobileVerify(String mobile) async {
+  await LCUser.requestMobilePhoneVerify(mobile);
+}
+
+Future<void> verifyMobile(String mobile, String verifyCode) async {
+  await LCUser.verifyMobilePhone(mobile, verifyCode);
+}
+
+Future<void> requestPasswordResetSMSCode(String mobile) async {
+  await LCUser.requestPasswordRestBySmsCode(mobile);
+}
+
+Future<void> resetPasswordBySMSCode(
+  String mobile,
+  String smscode,
+  String password,
+) async {
+  await LCUser.resetPasswordBySmsCode(mobile, smscode, password);
+}
+
+Future<LCUser> currentUser() async {
+  return await LCUser.getCurrent();
+}
